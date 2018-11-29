6670bff3082035329178416977891a0830815d59
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-11-29 04:59:22 +0100

    Added new button

--- src/components/Container/Modal/index.js
+++ src/components/Container/Modal/index.js
@@ -19,11 +19,17 @@ export default class Modal extends Component {
     return (
       <div className="modal-container">
         <button type="button" name="button" className="modal-btn" />
-          <i className="material-icons md-14" onClick={this.handleClick}>info_outline</i>
+          <i className="material-icons md-14" onClick={this.handleClick}>
+            info_outline
+          </i>
         </button>
 
         <dialog className="modal-about" open={this.state.modal}>
-          <button type="button" name="button" className="modal-cancel"></button>
+          <button type="button" name="button" className="modal-cancel">
+            <i className="material-icons sm-12" onClick={this.handleClick}>
+              clear
+            </i>
+          </button>
 
         </dialog>
       </div>)
