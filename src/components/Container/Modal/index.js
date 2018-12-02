2013ad02bd0b2a0bd24f6e8a90b5856afe7cfda7
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-12-02 05:22:12 +0100

    Finished the list

--- src/components/Container/Modal/index.js
+++ src/components/Container/Modal/index.js
@@ -42,6 +42,9 @@ export default class Modal extends Component {
               <li className="story">
                 My game will reset as soon as it's over so I can play again.
               </li>
+              <li className="story">
+                I can choose whether I want to play as X or O.
+              </li>
             </ul>
           </p>
         </dialog>
