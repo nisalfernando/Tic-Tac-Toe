49410de180939692f0b2e5a8d278b24aaf60bd1a
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-12-08 05:08:49 +0100

    Creating the logic behind the game

--- src/components/Presentational/Board/index.js
+++ src/components/Presentational/Board/index.js
@@ -2,4 +2,12 @@ import React from "react";
 
 import "./index.css";
 
-export default function Board({ board, playerMove }) {}
+export default function Board({ board, playerMove }) {
+  return (
+    <section className="board">
+      <div className="square one" data-value="0" onClick={() => playerMove(0)}>
+        <p className="element">{board[0]}</p>
+      </div>
+    </section>
+  );
+}
