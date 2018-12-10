2bc35253cd0521142fe230550377d3692494702f
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-12-10 06:12:44 +0100

    Adding instructions to the player

--- src/components/Presentational/Choose/index.js
+++ src/components/Presentational/Choose/index.js
@@ -5,6 +5,10 @@ export default function Choose({ chooseTeam, toggle }) {
   if (toggle) {
     return null;
   } else {
-    return <div className="choose-area" />;
+    return (
+      <div className="choose-area">
+        <h2 className="choose-title">Choose your team</h2>
+      </div>
+    );
   }
 }
