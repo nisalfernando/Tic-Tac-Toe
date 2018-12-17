c299da1f991d0cb030ea8822b774ebb6d619fc00
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-12-17 05:31:36 +0100

    Added second choosing button

--- src/components/Presentational/Choose/index.js
+++ src/components/Presentational/Choose/index.js
@@ -7,9 +7,12 @@ export default function Choose({ chooseTeam, toggle }) {
   } else {
     return (
       <div className="choose-area">
-        <h2 className="choose-title">Choose your team</h2>
+        <h2 className="choose-title">Choose your team:</h2>
         <button className="choose-btn" onClick={chooseTeam}>
           X
+        </button>
+        <button className="choose-btn" onClick={chooseTeam}>
+          O
         </button>
       </div>
     );
