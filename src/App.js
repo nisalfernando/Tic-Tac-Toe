736c9d789920aec6cbc82e92b9c14130aa5fd21f
Nisal Fernando <nisal.shiroma86@gmail.com>
2018-12-27 21:01:25 +0100

    Creating a connection among files

--- src/App.js
+++ src/App.js
@@ -3,3 +3,4 @@ import React, { Component } from "react";
 import "./App.css";
 import Modal from "./components/Container/Modal";
 import Choose from "./components/Presentational/Choose";
+import Board from "./components/Presentational/Board";
