d92b201cf579ef2cbc5fe4e2296ded79c956a5fd
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-01-18 20:53:26 +0100

    Coding a IF statement

--- src/App.js
+++ src/App.js
@@ -56,6 +56,8 @@ class App extends Component {
       (board[0] === team && board[1] === team && board[2] === team) ||
       (board[3] === team && board[4] === team && board[5] === team) ||
       (board[6] === team && board[7] === team && board[8] === team) ||
+      // Vertical
+      (board[0] === team && board[3] === team && board[6] === team) ||
     )
   }
 }
