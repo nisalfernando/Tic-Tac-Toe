92c458c22dbde32acf6422a90d91813fb16d09ea
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-02-01 19:10:01 +0100

    Modified a file

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
         <Team toggle={this.state.chosenTeam}
           player={this.state.player}
           computer={this.state.computer} />
-        <Result message />
+        <Result message={} />
       </div>
     )
   }
