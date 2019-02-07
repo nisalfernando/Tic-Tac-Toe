51a3ad22380afe3c35e3209e795244e69a0aac3e
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-02-07 19:15:27 +0100

    Fixing some bugs

--- src/components/Presentational/Team/index.js
+++ src/components/Presentational/Team/index.js
@@ -7,7 +7,7 @@ export default function Team({ toggle, player, computer }) {
   if (toggle) {
     return (
       <div className='team-area' id='team-area'>
-        <p className='teams'>Player:
+        <p className='team'>Player:
         <span className={'. ' + player}> {player} </span></p>
         <p className='team'>Computer:<span className={'. ' + computer}> {computer} </span></p>
       </div>
